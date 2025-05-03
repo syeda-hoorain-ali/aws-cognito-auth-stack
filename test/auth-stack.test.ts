87771f3588bdeb0d6096a504db906dabf18eed7e
Syeda Hoorain Ali <syedahoorainali8@gmail.com>
2025-05-03 23:20:31 +0500

    Initial commit

---
+++ test/auth-stack.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AuthStack from '../lib/auth-stack-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/auth-stack-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AuthStack.AuthStackStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
